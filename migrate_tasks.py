5f5b371662a99b68ebd4bec2a894c3671328358a
Adam Reese <areese801@gmail.com>
2022-12-19 15:31:42 -0700

    got todoist functionality working

--- migrate_tasks.py
+++ migrate_tasks.py
@@ -11,6 +11,7 @@ take advantage of the very nature of sync.
 """
 import os.path
 import sys
+import shutil
 
 import todoist
 from find_tasks import find_tasks
@@ -38,6 +39,8 @@ def migrate_tasks(parent_directory:str = '~/Obsidian'):
 	todoist_api_token = todoist.get_api_token()
 	todoist_tasks = todoist.get_todoist_tasks(todoist_api_token=todoist_api_token)
 
+	recently_modified_files = [] #Tracks files that were modified by current invocation.  To not-skip due to modify time.
+
 	for task_dict in tasks_from_markdown_files:
 
 		"""
@@ -46,17 +49,23 @@ def migrate_tasks(parent_directory:str = '~/Obsidian'):
 		Todoist prematurely.  For example, if this program was scheduled on a cron job
 		"""
 
+		markdown_file_name = task_dict['file_name']
+
 		# take note of the current timestamp in UTC
-		right_now_utc_timestamp = datetime.datetime.now(timezone.utc).timestamp()
+		right_now = datetime.datetime.now(timezone.utc)
+		right_now_utc_timestamp = right_now.timestamp()
 
 		# take note of the timestamp on the file
-		file_last_modified_timestamp = os.path.getmtime(task_dict['file_name'])
+		file_last_modified_timestamp = os.path.getmtime(markdown_file_name)
 
 		time_diff_sec = right_now_utc_timestamp - file_last_modified_timestamp
-		if file_last_modified_timestamp > right_now_utc_timestamp or time_diff_sec < 120:
+		if file_last_modified_timestamp > right_now_utc_timestamp or time_diff_sec < 60:
 			# TODO:  Read this seconds threshold out of a config file and allow the user bypass this behavior entirely
 			# File is too new.  Skip it for now
-			continue
+			if markdown_file_name not in recently_modified_files:
+				# We might have just modified a file due to another to-do item, changing its TS.
+				# It's ok to modify again in such cases.
+				continue
 
 		"""
 		For good measure, bump the list of existing tasks from todoist up against that which is in scope right now
@@ -70,15 +79,17 @@ def migrate_tasks(parent_directory:str = '~/Obsidian'):
 				break
 
 		if matching_task_in_todoist is True:
-			print(f"The task parsed from a markdown file '{task_dict['task']}' seems to be a duplicate of a task"
-			      f" that already exists in todoist, '{tdt.content}'.  As such, it will be skipped over.", file=sys.stderr)
+			# TODO:  Read behavior for this out of a config file to enable or disable
+			print(f"The task '{task_dict['task']}' parsed from the markdown file '{markdown_file_name}' seems to be a "
+			      f"duplicate of a task that already exists in todoist, '{tdt.content}'.  "
+			      f"As such, it will be skipped over.", file=sys.stderr)
 			continue
 
 		"""
 		Make the task in todoist.  This is the moment we've been waiting for!
 		"""
 		task_content = task_dict['task']
-		original_file_name = os.path.basename(task_dict['file_name'])
+		original_file_name = os.path.basename(markdown_file_name)
 		task_description = f"This todo item was parsed from [{original_file_name}]({task_dict['obsidian_uri']}). " \
 		               f"Note that this link may be broken if the file was renamed or moved."
 
@@ -114,10 +125,41 @@ def migrate_tasks(parent_directory:str = '~/Obsidian'):
 
 		#TODO:  Drop these helper messages.  Or don't
 		print("\nThe program will find and replace the following:")
-		print(f"\tWithin the file '{task_dict['file_name']}'")
+		print(f"\tWithin the file '{markdown_file_name}'")
 		print(f"\tThis string will be sought:            {task_dict['original_string']}")
 		print(f"\tWhich will be replaced by the string:  {replacement_todo_string}")
-		print("!")
+
+		# Create a backup copy of the file before modifying it
+		#TODO:  Probably safe to comment this out or disable via config after having used this tool for a while
+		backup_file_name = f"{markdown_file_name}.{right_now.strftime('%Y-%m-%d')}.bak"
+		if not os.path.isfile(backup_file_name):
+			shutil.copy(src=markdown_file_name, dst=backup_file_name)
+			print(f"Created a backup file before modifying to-do item in place in the original.  Backup file name is:  '{backup_file_name}'")
+		else:
+			print(f"A backup file '{backup_file_name}' already exists.  Will not create another backup file")
+
+		"""
+		Replace the original line in the file with a to-do item on it with the new field that 
+		show's it's been migrated to todoist
+		"""
+		# Read the lines of the file first
+		with open(markdown_file_name, 'r') as f:
+			lines = f.readlines()
+
+		# Modify the lines
+		new_lines = []
+		for line in lines:
+			if line.startswith(task_dict['original_string']):
+				new_lines.append(replacement_todo_string)
+			else:
+				new_lines.append(line.rstrip())  # We don't want trailing \n char or we'll get doubles when we join
+		new_file_data = "\n".join(new_lines)  #TODO:  Might need to do a check here to place nicely with windows
+
+		# Replace the lines of the original file with the new lines
+		with open(markdown_file_name, 'w') as f:
+			f.write(new_file_data)
+			f.close()
+		recently_modified_files.append(markdown_file_name) # Helps us merge many tasks from the same file in without TS check
 
 
 		# To Do:  Cross out the todo in the markdown file
